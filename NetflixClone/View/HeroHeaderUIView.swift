afc76d7a802e029c687f59291bc2c6ba25e0b0f8
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-01 19:25:04 -0600

    Play button

--- NetflixClone/View/HeroHeaderUIView.swift
+++ NetflixClone/View/HeroHeaderUIView.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
     
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -21,6 +30,8 @@ class HeroHeaderUIView: UIView {
         super.init(frame: frame)
         addSubview(heroImageView)
         addGradient()
+        addSubview(playButton)
+        applyConstraints()
     }
     
     override func layoutSubviews() {
@@ -36,6 +47,16 @@ class HeroHeaderUIView: UIView {
         ]
         gradientLayer.frame = bounds
         layer.addSublayer(gradientLayer)
+    }
+    
+    private func applyConstraints() {
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 125),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+            playButton.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraints)
     }
 
     required init?(coder: NSCoder) {
