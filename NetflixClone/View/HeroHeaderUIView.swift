6385a3cf86411c307b5a4b66e6fd6f86837e5f2d
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-01 19:14:48 -0600

    Gradient added to the Hero HeaderView

--- NetflixClone/View/HeroHeaderUIView.swift
+++ NetflixClone/View/HeroHeaderUIView.swift
@@ -13,17 +13,29 @@ class HeroHeaderUIView: UIView {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
         return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
+        addGradient()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         heroImageView.frame = bounds
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
     }
 
     required init?(coder: NSCoder) {
