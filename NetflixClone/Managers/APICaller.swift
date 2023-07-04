c03b713c3cddb6c7b31e6c47f140f90d54b9626c
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-04 16:30:29 -0600

    Tv model and getTrendingMovies function

--- NetflixClone/Managers/APICaller.swift
+++ NetflixClone/Managers/APICaller.swift
@@ -20,7 +20,7 @@ class APICaller {
     static let shared = APICaller()
     
     func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
-        guard let url = URL(string: "\(Constants.baseUrl)/3/trending/all/day?api_key=\(Constants.API_KEY)")
+        guard let url = URL(string: "\(Constants.baseUrl)/3/trending/movie/day?api_key=\(Constants.API_KEY)")
         else { return }
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
@@ -30,6 +30,22 @@ class APICaller {
                 completion(.success(result.results))
             } catch {
                 completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    func getTrendingTvs(completion: @escaping (Result<[String], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseUrl)/3/trending/tv/day?api_key=\(Constants.API_KEY)")
+        else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error  == nil else { return }
+            do {
+                let result = try JSONDecoder().decode(TrendingTvResponse.self, from: data)
+                print(result)
+            } catch {
+                print(error.localizedDescription)
             }
         }
         task.resume()
