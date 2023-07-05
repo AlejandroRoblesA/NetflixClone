b258e34b36df1701c99b9855193c8f0f51f64f19
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-05 13:19:20 -0600

    TitleCollectionViewCell file

---
+++ NetflixClone/View/TitleCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TitleCollectionViewCell.swift
+//  NetflixClone
+//
+//  Created by Alejandro Robles on 05/07/23.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+}
