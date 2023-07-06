a2b5f59602aa85d3a445bffdaf6f61923579de0a
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-06 13:44:11 -0600

    TitlePosterUIImage contraints Activated

---
+++ NetflixClone/ViewModel/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Alejandro Robles on 06/07/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterUrl: String
+}
