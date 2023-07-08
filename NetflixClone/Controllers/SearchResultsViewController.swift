eafaf780ab2d67bd9e85e301f1655618e9ab403b
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-08 16:09:47 -0600

    SearchResultsViewController file

---
+++ NetflixClone/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  NetflixClone
+//
+//  Created by Alejandro Robles on 08/07/23.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+
+}
