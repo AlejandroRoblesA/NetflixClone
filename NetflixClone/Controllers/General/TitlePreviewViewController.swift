a922ee8cca09628e3ad8be3eefad39826594f81f
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-13 19:28:08 -0600

    TitlePreviewViewController Labels and downloadButton

---
+++ NetflixClone/Controllers/General/TitlePreviewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TitlePreviewViewController.swift
+//  NetflixClone
+//
+//  Created by Alejandro Robles on 13/07/23.
+//
+
+import UIKit
+import WebKit
+
+class TitlePreviewViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    private let overViewLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
+        button.setTitle("Download", for: .normal)
+        return button
+    }()
+
+    private let webView = WKWebView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
