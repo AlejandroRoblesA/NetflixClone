87033736a123ae6ff542ad8ec41def0c2545cac0
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-07-15 19:13:35 -0600

    SearchResultsViewControllerDelegate

--- NetflixClone/Controllers/General/SearchResultsViewController.swift
+++ NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol SearchResultsViewControllerDelegate: AnyObject {
+    func searchResultViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel)
+}
+
 class SearchResultsViewController: UIViewController {
     
     public let searchResultsCollectionView: UICollectionView = {
@@ -19,6 +23,7 @@ class SearchResultsViewController: UIViewController {
     }()
 
     public var titles: [Title] = [Title]()
+    public weak var delegate: SearchResultsViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,5 +52,20 @@ extension SearchResultsViewController: UICollectionViewDelegate, UICollectionVie
         let title = titles[indexPath.row]
         cell.configure(with: title.poster_path ?? "")
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let title = titles[indexPath.row]
+        let titleName = title.original_title ?? ""
+        APICaller.shared.getMovie(with: titleName) { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                self?.delegate?.searchResultViewControllerDidTapItem(TitlePreviewViewModel(title: title.original_title ?? "", youtubeView: videoElement, titleOverview: title.overview ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
     }
 }
